157b7b184cb9cca6e4343b17e204a7708e0d1805
Usman Eletu <usmaneletu2@gmail.com>
2025-11-14 12:04:33 +0100

    deploy

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -39,14 +39,14 @@ const Login = () => {
       // ✅ Handle errors properly
       if (userCredential?.error) {
         console.log("error1")
-        showError(userCredential.error.message || "Authentication failed.");
+        showError(userCredential.error.message );
         setLoading(false);
         return;
       }
 
       if (!userCredential?.user) {
         console.log("error2")
-        showError("Authentication failed.");
+        
         setLoading(false);
         return;
       }
